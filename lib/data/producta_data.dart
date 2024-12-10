44a09ed5383b5615e69c266dbda977f7d334cf82
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-12-10 10:24:02 +0530

    Added Data

---
+++ lib/data/producta_data.dart
@@ -0,0 +1,75 @@
+class ProductaData {
+  static List<Map<String, dynamic>> groceryProducts = [
+    {
+      'id': 01,
+      'name': 'Mango',
+      'description': 'Alphanso Mangos',
+      'price': 1000,
+      'imageUrl': 'https://rukminim2.flixcart.com/image/850/1000/l0jwbrk0/plant-sapling/e/g/i/no-annual-yes-mango-plant-5024-1-natikrd-original-imagcbdjhnnx8jq4.jpeg?q=20&crop=false',
+    },
+    {
+      'id': 02,
+      'name': 'Apple',
+      'description': 'Fresh red apples',
+      'price': 120,
+      'imageUrl': 'https://freshindiaorganics.com/cdn/shop/products/Apples.jpg?v=1686739530',
+    },
+    {
+      'id': 03,
+      'name': 'Banana',
+      'description': 'Organic Bananas',
+      'price': 60,
+      'imageUrl': 'https://d2jx2rerrg6sh3.cloudfront.net/image-handler/picture/2018/2/banana.jpg',
+    },
+    {
+      'id': 04,
+      'name': 'Grapes',
+      'description': 'Fresh Grapes',
+      'price': 200,
+      'imageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1eWH5kQrsjt5yvMmadtDoB24cM-772-p1HA&s',
+    },
+    {
+      'id': 05,
+      'name': 'Sugar Cane',
+      'description': 'Healthy Sugar cane',
+      'price': 30,
+      'imageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfDA-glZvBtQ1owScujoNrCFGfOA1yaXBFbw&s',
+    },
+    {
+      'id': 06,
+      'name': 'Oranges',
+      'description': 'Nagpur Oranges',
+      'price': 50,
+      'imageUrl': 'https://www.health.com/thmb/4CdxwkcePxrn-bUOLaKsd1ThRm4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TypesOfOranges-f95153e786554ba5b6da0370387ee563.jpg',
+    },
+    {
+      'id': 07,
+      'name': 'Cherry',
+      'description': 'Kashmir Cherries',
+      'price': 150,
+      'imageUrl': 'https://www.simplyrecipes.com/thmb/qM4OHyTEdSjpHbfhIJAk-fCWK6k=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-How-Store-Recipes-LEAD-OPTION-04-0bf195d4fee44090a3b4b89b6cfbf3e0.jpg',
+    },
+    {
+      'id': 08,
+      'name': 'Peach',
+      'description': 'Fresh Paris',
+      'price': 80,
+      'imageUrl': 'https://media.self.com/photos/5b75ad5728dfab53ee567688/16:9/w_5088,h_2862,c_limit/GettyImages-614938268.jpg',
+    },
+    {
+      'id': 09,
+      'name': 'Water melom',
+      'description': 'Fresh water melon',
+      'price': 100,
+      'imageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7KDumi-cWzQhj_NOTFTlWHWpGgVk9kF1oIg&s',
+    },
+    
+    {
+      'id': 10,
+      'name': 'Strawberry',
+      'description': 'Mahabaleshwar Strawberry',
+      'price': 110,
+      'imageUrl': 'https://clv.h-cdn.co/assets/15/22/2048x2048/square-1432664914-strawberry-facts1.jpg',
+    },
+  ];
+}
